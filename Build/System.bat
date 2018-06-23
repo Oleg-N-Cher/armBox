717e5c09198b7ec181bcc67c09245edc96539b64
Oleg N. Cher <allot@bk.ru>
2018-06-23 03:17:14 +0300

    * Build

--- Build/System.bat
+++ Build/System.bat
@@ -2,22 +2,32 @@
 IF NOT "%XDev%"=="" SET PATH=%XDev%\WinDev\Bin\MinGW\bin
 CD ..\Obj48
 
-SET StripDll=-nostartfiles DllMain.o -Wl,-e_DllMain@12
-SET CC=gcc.exe -I..\C -m32 -s -Os -g0 -fvisibility=hidden -finline-small-functions -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -Wl,--gc-sections
-
-%CC% -fPIC -c ..\C\DllMain.c Kernel.c Math.c Strings.c
-IF errorlevel 1 PAUSE
+SET StripDll=-nostartfiles ..\C\DllMain.c -Wl,-e_DllMain@12
+SET StripExe=-nostartfiles ..\C\ExeMain.c -Wl,-e__WinMain
+SET LibReloc=%XDev%\WinDev\Bin\MinGW\i686-w64-mingw32\lib\libmingw32.a
+SET CC=gcc.exe -fPIC -I..\C -m32 -s -Os -g0 -fvisibility=hidden -finline-small-functions -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -Wl,--gc-sections
+SET AR=ar.exe -rc CPfront.a
+IF EXIST CPfront.a DEL CPfront.a
 
 :: Kernel
-%CC% %StripDll% -fPIC Kernel.o Kernel.def -static-libgcc -shared -o Kernel.dll CPfront.a
+%CC% -c ..\C\SYSTEM.c Types.c Kernel.c StdLog.c Math.c Strings.c Files.c
+IF errorlevel 1 PAUSE
+%AR% SYSTEM.o Types.o Kernel.o StdLog.o Math.o Strings.o Files.o
+
+:: Init
+%CC% -fPIC %StripExe% ..\C\Init.c -I. armBox.def -o armBox.exe -Wl,--out-implib,armBox.a CPfront.a
 IF errorlevel 1 PAUSE
 
 :: Math
-%CC% %StripDll% -fPIC Math.o Math.def -static-libgcc -shared -o Math.dll CPfront.a
+%CC% %StripDll% Math.o armBox.a -static-libgcc -shared -o Math.dll -Wl,--out-implib,Math.a
 IF errorlevel 1 PAUSE
 
 :: Strings
-%CC% %StripDll% -fPIC Strings.o -static-libgcc -shared -o Strings.dll CPfront.a
+%CC% %StripDll% Strings.o Math.a armBox.a -static-libgcc -shared -o Strings.dll -Wl,--out-implib,Strings.a
+IF errorlevel 1 PAUSE
+
+:: Files
+%CC% %StripDll% Files.o armBox.a -static-libgcc -shared -o Files.dll -Wl,--out-implib,Files.a
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.dll) DO MOVE /Y %%i ..\System\Code >NUL

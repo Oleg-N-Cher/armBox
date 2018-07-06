6d3bc071b2c610d103fc337e08b62b2e32713caa
Oleg N. Cher <allot@bk.ru>
2018-07-06 07:38:18 +0300

    * Build

--- Build/System.bat
+++ Build/System.bat
@@ -1,11 +1,14 @@
+@SET Debug=-g
+
 @ECHO OFF
 IF NOT "%XDev%"=="" SET PATH=%XDev%\WinDev\Bin\MinGW\bin
 CD ..\Obj48
 
+IF "%Debug%"=="" SET Debug=-s -Os
 SET StripDll=-nostartfiles ..\C\DllMain.c -Wl,-e_DllMain@12
 SET StripExe=-nostartfiles ..\C\ExeMain.c -Wl,-e__WinMain
 SET LibReloc=%XDev%\WinDev\Bin\MinGW\i686-w64-mingw32\lib\libmingw32.a
-SET CC=gcc.exe -fPIC -I..\C -m32 -s -Os -fvisibility=hidden -fno-exceptions -ffunction-sections -Wl,--gc-sections
+SET CC=gcc.exe -fPIC -I..\C -m32 %Debug% -fvisibility=hidden -fno-exceptions -ffunction-sections -Wl,--gc-sections
 SET AR=ar.exe -rc CPfront.a
 IF EXIST CPfront.a DEL CPfront.a
 

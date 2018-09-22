42bbe3fc320ec81fd7feb933dbe6d291edba2f38
Oleg N. Cher <allot@bk.ru>
2018-09-22 09:23:42 +0300

    + C/_gtk.h

---
+++ C/_gtk.h
@@ -0,0 +1,30 @@
+#ifndef GTK__h
+#define GTK__h
+
+#include <gtk/gtk.h>
+#include <pango/pango.h>
+#include "SYSTEM.h"
+
+
+typedef SHORTCHAR String;
+
+
+typedef GdkColormap GdkColormap__rec;
+typedef GdkCursor GdkCursor__rec;
+typedef GdkDrawable GdkDrawable__rec;
+typedef GtkDrawingArea GtkDrawingArea__rec;
+typedef GtkFixed GtkFixed__rec;
+typedef GdkGC GdkGC__rec;
+typedef GtkMenu GtkMenu__rec;
+typedef GtkObject GtkObject__rec;
+typedef GdkPixmap GdkPixmap__rec;
+typedef GtkWidget GtkWidget__rec;
+typedef PangoAttribute PangoAttribute__rec;
+typedef PangoAttrList PangoAttrList__rec;
+typedef PangoContext PangoContext__rec;
+typedef PangoFontDescription PangoFontDescription__rec;
+typedef PangoFontMetrics PangoFontMetrics__rec;
+typedef PangoLayout PangoLayout__rec;
+
+
+#endif

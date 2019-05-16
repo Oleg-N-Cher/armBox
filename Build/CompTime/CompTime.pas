5312ba1893f0a0e92c13da761afea7933abafdfe
unknown <allot@bk.ru>
2019-05-16 03:02:10 +0300

    * Build/CompTime

--- Build/CompTime/CompTime.pas
+++ Build/CompTime/CompTime.pas
@@ -1,19 +1,25 @@
 (*
-	{2019, 5, 13, 2, 56, 38}, /* compTime */ 
+	{2019, 5, 13, 22, 34, 56}, /* compTime */ 
 *)
 
 PROGRAM CompTime;
 USES Kol;
 
+CONST
+  Stamp = '{2019, 5, 13, 22, 34, 56}';
 VAR
   bin: AnsiString;
-  position: INTEGER;
-
+  position, len: INTEGER;
 BEGIN
   bin := StrLoadFromFile(ParamStr(1)); //Write(' size ', LENGTH(bin));
-  position := POS('}, /* compTime */', bin);
-  IF position > 0 THEN BEGIN Write(' found at ', position);
-    DELETE(bin, position - 24, 41);
+  position := POS(', /* compTime */', bin);
+  IF position > 0 THEN BEGIN
+    // Write(' found at ', position);
+    len := 0;
+    WHILE bin[position - len] <> '{' DO INC(len);
+    DELETE(bin, position - len, len);
+    INSERT(Stamp, bin, position - len);
+    DELETE(bin, LENGTH(bin), 1);
     StrSaveToFile(ParamStr(1), bin)
   END
 END {CompTime}.
